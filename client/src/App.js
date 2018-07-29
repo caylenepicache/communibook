a63b13da45eef0f608114bb0b4e1e8d621389d83
unknown <caylenepicache@gmail.com>
2018-07-28 20:17:40 -0700

    can route navbar onto react

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
-import React /*,  { Component } */ from 'react';
+import React,  { Component } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import NavBar from './components/layouts/NavBar';
 import './App.css';
-import Button from '@material-ui/core/Button';
 
-class App extends React.Component {
+
+class App extends Component {
   render() {
     return (
-        <div className="App">
-          <h1>Hello world</h1>
-        <Button variant="contained" color="primary">
-        Hello World
-       </Button>
-       </div>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={NavBar} />
+        </Switch>
+      </HashRouter>
     );
   }
 }
---
+++ client/src/components/layouts/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+//import { NavLink } from 'react-router-dom';
+import './NavBar.css';
+import Button from '@material-ui/core/Button'
+
+
+const NavBar = () => (
+  <div id="NavBar">
+    <div className="App">
+          <h1>Hello Im Here</h1>
+          <h1>Hello world</h1>
+        <Button variant="contained" color="primary">
+        Hello World
+       </Button>
+       </div>
+       </div>
+    );
+
+
+export default NavBar;

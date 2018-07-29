cf928dac1e38fa38359902e5102b507c29f3aad4
unknown <caylenepicache@gmail.com>
2018-07-28 20:02:48 -0700

    can render button and text on react

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
+import React /*,  { Component } */ from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import NavBar from './components/layouts/NavBar';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello world</h1>
-      </div>
+        <div className="App">
+          <h1>Hello world</h1>
+        <Button variant="contained" color="primary">
+        Hello World
+       </Button>
+       </div>
     );
   }
 }
